ee43fe7e09bb5f7f75fb0986325599d5be98e8d1
Michał Wypych <micwypych@gmail.com>
2017-01-05 01:50:09 +0100

    Removes commented code

--- src/main/scala/Library.scala
+++ src/main/scala/Library.scala
@@ -3,33 +3,13 @@ import scala.scalajs.js.JSApp
 
 object Library extends JSApp {
 
+   def h1(str: String) :String = {
+     "<h1>"+str+"</h1>"
+   }
+
 
    def main(): Unit = {
-/*val examples = Seq(
-    "UbLKYAK/1" -> "Hilbert curve",
-    "T3iPnC/1" -> "Circe JSON example",
-    "eYtp3Lu/0" -> "Monix example",
-    "KOsXSKv/0" -> "Oscilloscope",
-    "4beVrVc/1" -> "Ray Tracer"
-  )
-  
-println(div(
-  h1("Welcome to ScalaFiddle!"),
-  p("""ScalaFiddle is an online playground for writing 
-    and sharing Scala code. Get started by writing your Scala code
-    in the editor on the left and press Ctrl/Cmd+Enter 
-    (or click the Run button) to compile and run it. The result
-    will be shown in this right hand panel.
-  """),
-  p(a(target := "_blank", href := s"/", "Create a new fiddle")),
-  h3("Examples"),
-  p("Alternatively you can take a look at some example ScalaFiddles to give you inspiration!"),
-  ul(
-    for((id, name) <- examples) 
-      yield li(a(target := "_blank", href := s"/sf/$id", name))
-  )
-))*/
-     println("<h1>Hello World!</h1>")
+     println(h1("Hello World!"))
 }
 
 }
